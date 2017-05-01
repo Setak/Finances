a521879cbd1e84218509f87350bf7ec65fa368c8
PaulM <paulmk1985@gmail.com>
2017-05-01 19:46:53 -0400

    new changes - home

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,9 @@
  */
 import java.text.DecimalFormat;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
-
-    static Scanner userInput = new Scanner(System.in);
 
     private Float totalAcct;
 
@@ -20,7 +19,11 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Scanner userInput = new Scanner(System.in);
+
         float totalAcct;
+        ArrayList<String> billName = new ArrayList<String>();
+        ArrayList<Float> billPay = new ArrayList<Float>();
 
         DecimalFormat df = new DecimalFormat();  // sets DecimalFormat Class
         df.setMaximumFractionDigits(2);          // makes it so using this format forces 2 places after decimal
@@ -37,8 +40,15 @@ public class Main {
 
         System.out.print("\n");
 
+        System.out.print("Enter a bill name here: ");
+        billName.add(userInput.next());
+        System.out.print("Enter how much to pay " + billName + " this month.");
+        billPay.add(userInput.nextFloat());
+
         totalAcct = checkingAcct.getCheckingAcct() + savingsAcct.getSavingsAcct();
         System.out.println("You have $" + df.format(totalAcct) + " in your accounts.");
+
+        System.out.println("You owe " + billPay);
 
     }
 
