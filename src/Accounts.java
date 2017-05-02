ec843d93a03a31ac56a4c92edc17efe601cf34d7
PaulM <paulmk1985@gmail.com>
2017-05-01 21:31:45 -0400

    new changes - home

---
+++ src/Accounts.java
@@ -0,0 +1,26 @@
+/**
+ * Created by PaulM on 5/1/2017.
+ */
+public class Accounts {
+
+    private float checkingAcct;
+    private float savingsAcct;
+
+
+    public Float getCheckingAcct() {
+        return checkingAcct;
+    }
+
+    public Float getSavingsAcct() {
+        return savingsAcct;
+    }
+
+    public void setCheckingAcct(float newCheckingAcct) {
+        checkingAcct = newCheckingAcct;
+    }
+
+    public void setSavingsAcct(float newSavingsAcct) {
+        savingsAcct = newSavingsAcct;
+    }
+
+}
