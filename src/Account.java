b88d569dcba8bc82277cea026e5a885473d79b17
PaulM <paulmk1985@gmail.com>
2017-05-01 22:25:16 -0400

    new changes - home

--- src/Account.java
+++ src/Account.java
@@ -1,26 +1,34 @@
 /**
  * Created by PaulM on 5/1/2017.
  */
-public class Accounts {
+public class Account {
 
     private float checkingAcct;
     private float savingsAcct;
+    private float totalAcct;
 
-
-    public Float getCheckingAcct() {
+    public float getCheckingAcct() {
         return checkingAcct;
-    }
-
-    public Float getSavingsAcct() {
-        return savingsAcct;
     }
 
     public void setCheckingAcct(float newCheckingAcct) {
         checkingAcct = newCheckingAcct;
     }
 
+    public float getSavingsAcct() {
+        return savingsAcct;
+    }
+
     public void setSavingsAcct(float newSavingsAcct) {
         savingsAcct = newSavingsAcct;
+    }
+
+    public float getTotalAcct() {
+        return totalAcct;
+    }
+
+    public void setTotalAcct(float newTotalAcct) {
+        totalAcct = newTotalAcct;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,13 @@ import java.util.ArrayList;
 
 public class Main {
 
-    private Float totalAcct;
-
-    public float getTotalAcct() {
-        return totalAcct;
-    }
-
-    public void setTotalAcct(float newTotalAcct) {
-        totalAcct = newTotalAcct;
-    }
-
     public static void main(String[] args) {
 
         Scanner userInput = new Scanner(System.in);
+        Account checkingAcct = new Account();
+        Account savingsAcct = new Account();
+        Account totalAcct = new Account();
 
-        float totalAcct;
         ArrayList<String> billName = new ArrayList<String>();
         ArrayList<Float> billPay = new ArrayList<Float>();
 
@@ -29,12 +21,10 @@ public class Main {
         df.setMaximumFractionDigits(2);          // makes it so using this format forces 2 places after decimal
 
         System.out.print("Enter Checking Account Amount: ");
-        CheckingAcct checkingAcct = new CheckingAcct();
         checkingAcct.setCheckingAcct(userInput.nextFloat());
         System.out.println("You have " + df.format(checkingAcct.getCheckingAcct()) + " in your Checking Account.");
 
         System.out.print("Enter Savings Account Amount: ");
-        SavingsAcct savingsAcct = new SavingsAcct();
         savingsAcct.setSavingsAcct(userInput.nextFloat());
         System.out.println("You have " + df.format(savingsAcct.getSavingsAcct()) + " in your Savings Account.");
 
@@ -45,8 +35,8 @@ public class Main {
         System.out.print("Enter how much to pay " + billName + " this month: ");
         billPay.add(userInput.nextFloat());
 
-        totalAcct = checkingAcct.getCheckingAcct() + savingsAcct.getSavingsAcct();
-        System.out.println("You have $" + df.format(totalAcct) + " in your accounts.");
+        totalAcct.setTotalAcct(checkingAcct.getCheckingAcct() + savingsAcct.getSavingsAcct());
+        System.out.println("You have $" + df.format(totalAcct.getTotalAcct()) + " in your accounts.");
 
         System.out.println("You owe " + billPay + " on " + billPay.size() + " bills.");
 
