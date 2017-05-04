2223f298b0064cd5c8736e9f9c2269d08e00d1f2
PaulM <paulmk1985@gmail.com>
2017-05-03 21:36:43 -0400

    new changes - home

--- src/Account.java
+++ src/Account.java
@@ -3,32 +3,32 @@
  */
 public class Account {
 
-    private float checkingAcct;
-    private float savingsAcct;
-    private float totalAcct;
+    private float checkingAcct;                                                                                         // Initializes private float variable for Checking Account
+    private float savingsAcct;                                                                                          // Initializes private float variable for Savings Account
+    private float totalAcct;                                                                                            // Initializes private float variable for Account Totals when Checking and Savings are added
 
-    public float getCheckingAcct() {
-        return checkingAcct;
-    }
+    public float getCheckingAcct() {                                                                                    // Starts Getter for checkingAcct
+        return checkingAcct;                                                                                            // returns checkingAcct variable to be used in Main
+    }                                                                                                                   // Ends Getter for checkingAcct
 
-    public void setCheckingAcct(float newCheckingAcct) {
-        checkingAcct = newCheckingAcct;
-    }
+    public void setCheckingAcct(float newCheckingAcct) {                                                                // Starts Setter for checkingAcct
+        checkingAcct = newCheckingAcct;                                                                                 // assigns the float captured from Main to the checkingAcct variable
+    }                                                                                                                   // Ends Setter for checkingAcct
 
-    public float getSavingsAcct() {
-        return savingsAcct;
-    }
+    public float getSavingsAcct() {                                                                                     // Starts Getter for savingsAcct
+        return savingsAcct;                                                                                             // returns savingsAcct variable to be used in Main
+    }                                                                                                                   // Ends Getter for savingsAcct
 
-    public void setSavingsAcct(float newSavingsAcct) {
-        savingsAcct = newSavingsAcct;
-    }
+    public void setSavingsAcct(float newSavingsAcct) {                                                                  // Starts Setter for savingsAcct
+        savingsAcct = newSavingsAcct;                                                                                   // assigns the float captured from Main to the savingsAcct variable
+    }                                                                                                                   // Ends Setter for savingsAcct
 
-    public float getTotalAcct() {
-        return totalAcct;
-    }
+    public float getTotalAcct() {                                                                                       // Starts Getter for totalAcct
+        return totalAcct;                                                                                               // returns totalAcct variable to be used in Main
+    }                                                                                                                   // Ends Getter for totalAcct
 
-    public void setTotalAcct(float newTotalAcct) {
-        totalAcct = newTotalAcct;
-    }
+    public void setTotalAcct(float newTotalAcct) {                                                                      // Starts Setter for totalAcct
+        totalAcct = newTotalAcct;                                                                                       // assigns the float captured from Main to the totalAcct variable
+    }                                                                                                                   // Ends Setter for totalAcct
 
 }
--- src/Bills.java
+++ src/Bills.java
@@ -3,29 +3,29 @@
  */
 public class Bills {
 
-    private double billExpense;
-    private String billName;
-    private double billTotal;
+    private double billExpense;                                                                                         // Set private variable for the cost of the bill, billExpense
+    private String billName;                                                                                            // Set private variable for the name of the bill, billName
+    private double billTotal;                                                                                           // Set private variable for the total of all the bills stored
 
-    public double getBillTotal() {
-        return billTotal;
-    }
+    public double getBillTotal() {                                                                                      // Start Getter for billTotal variable
+        return billTotal;                                                                                               // returns the billTotal variable to be called from Main
+    }                                                                                                                   // Ends Getter for billTotal variable
 
-    public void setBillTotal(double newBillTotal) {
-        billTotal = newBillTotal;
-    }
+    public void setBillTotal(double newBillTotal) {                                                                     // Start Setter for billTotal
+        billTotal = newBillTotal;                                                                                       // Sets the billTotal variable to the passed in Double through Main
+    }                                                                                                                   // Ends Setter for billTotal
 
-    public double getBillExpense() {
-        return billExpense;
-    }
+    public double getBillExpense() {                                                                                    // Start Getter for billExpense
+        return billExpense;                                                                                             // returns the billExpense variable to be called from Main
+    }                                                                                                                   // Ends Getter for billExpense
 
-    public String getBillName() {
-        return billName;
-    }
+    public String getBillName() {                                                                                       // Start Getter for billName
+        return billName;                                                                                                // returns the billName variable to be called in Main
+    }                                                                                                                   // Ends Getter for billName
 
-    public void setBill(double newBillExpense, String newBillName) {
-        billExpense = newBillExpense;
-        billName = newBillName;
-    }
+    public void setBill(double newBillExpense, String newBillName) {                                                    // Start Setter for setBill, which will take the values for the bill, name and cost, and get it ready to be added to the ArrayList Bills
+        billExpense = newBillExpense;                                                                                   // Sets the billExpense variable based on the double captured in Main
+        billName = newBillName;                                                                                         // Sets the billName variable based on the string captured in Main
+    }                                                                                                                   // Ends Setter for setBill
 
 }
